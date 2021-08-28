e7bf71b5651e62c6709a8675d44855bf388aa1d1
Federico Galatolo <galatolo.federico@gmail.com>
2021-08-28 16:34:26 +0200

    bundle images

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const glob = require("glob");
 const HtmlWebpackPlugin = require("html-webpack-plugin")
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: glob.sync("./src/js/*.js"),
@@ -11,7 +12,12 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: "src/index.html"
-    })
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+          { from: "assets", to: "assets" }
+      ]
+  })
   ],
   devtool: 'inline-source-map',
   devServer: {
