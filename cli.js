b0b9d4b2c2ead5df50238369df8e835e26182885
Federico Galatolo <galatolo.federico@gmail.com>
2021-11-05 17:49:34 +0100

    implemented CLI script

---
+++ cli.js
@@ -0,0 +1,95 @@
+const { Command, Option } = require("commander")
+const program = new Command()
+const request = require("request")
+const fs = require("fs")
+
+const citation = require("./src/js/citation")
+const pdf = require("./src/js/pdf")
+const parseDOI = require("./src/js/doi")
+
+async function main(options){
+    let data = {
+        author: "",
+        title: "",
+        journal: "",
+        year: "",
+        volume: "",
+        pages: "",
+        publisher: "",
+        doi: "",
+        issn: "",
+    }
+
+    if (options.doi !== undefined){
+        console.log(options.doi)
+        doi = options.doi.replace("https://doi.org/", "")
+        data = await (new Promise((res, rej) => {
+            request(`https://api.crossref.org/works/${doi}`, (err, resp, body) => {
+                if (!err && resp.statusCode == 200) {
+                    body = JSON.parse(body)
+                    data = parseDOI(body)
+                    res(data)
+                } else {
+                    console.log(`Error parsing DOI ${doi}`)
+                    rej(err)
+                }
+            })
+        }))
+    }
+
+    for ([key, value] of Object.entries(options)) data[key] = value
+
+    doc = undefined;
+
+    if(options.type == "citation"){
+        bibtex = citation.buildBibtex(data)
+        apa = citation.buildAPA(data)
+
+        doc = pdf.buildCitationPDF({
+            unipi: fs.readFileSync("./assets/unipi-logo.png").toString("base64"),
+            dii: fs.readFileSync("./assets/dii-logo.png").toString("base64"),
+            mlpi: fs.readFileSync("./assets/mlpi-logo.png").toString("base64"),
+            title: data.title,
+            author: data.author,
+            bibtex: bibtex,
+            apa: apa
+        })
+    }
+    if(options.type == "preprint"){
+        doc = pdf.buildPreprintPDF({
+            unipi: fs.readFileSync("./assets/unipi-logo.png").toString("base64"),
+            dii: fs.readFileSync("./assets/dii-logo.png").toString("base64"),
+            mlpi: fs.readFileSync("./assets/mlpi-logo.png").toString("base64"),
+            title: data.title,
+            author: data.author,
+            notice_link: options.preprint_link
+        })
+    }
+
+    doc.save(options.output)
+}
+
+program
+.option("--doi <doi>", "paper DOI to fetch")
+.option("--title <title>", "override paper title")
+.option("--author <author>", "override paper author")
+.option("--journal <journal>", "override paper journal")
+.option("--year <year>", "override paper year")
+.option("--volume <volume>", "override paper volume")
+.option("--pages <pages>", "override paper pages")
+.option("--publisher <publisher>", "override paper publisher")
+.option("--issn <issn>", "override paper issn")
+.addOption(
+    new Option("--type <type>", "Page to generate")
+    .choices(["citation", "preprint"])
+    .default("citation")
+)
+.option("--output <output>", "output file", "citation.pdf")
+.option("--preprint_link <link>", "Preprint link", "http://mlpi.ing.unipi.it/")
+
+
+
+program.parse(process.argv);
+options = program.opts();
+
+main(options)
--- src/js/pdf.js
+++ src/js/pdf.js
@@ -19,17 +19,15 @@ function splitString(str, n){
 }
 
 
-function buildPDF(args){
+function buildCitationPDF(args){
     const doc = new jspdf.jsPDF({
         orientation: "landscape",
         unit: "mm",
         format: "a4"
     })
 
-    const rect_padding = 7
     const banner_height = 40
     const a4 = [297, 210]
-    const max_title_chars = 10
     const notice = "This is a preprint. Please cite using:"
     const title = splitString(args.title, 60).join("\n")
     const author = splitString(args.author, 90).join("\n")
@@ -83,4 +81,55 @@ function buildPDF(args){
 }
 
 
-module.exports = buildPDF
+
+function buildPreprintPDF(args){
+    const doc = new jspdf.jsPDF({
+        orientation: "landscape",
+        unit: "mm",
+        format: "a4"
+    })
+
+    const banner_height = 40
+    const a4 = [297, 210]
+    const notice = "This is a preprint."
+    const link = "Please export an up-to-date reference from: "+args.notice_link
+    const title = splitString(args.title, 60).join("\n")
+    const author = splitString(args.author, 90).join("\n")
+    
+    let author_height = 100
+    if(splitString(args.title, 60).length > 2){
+        author_height += 7
+    }
+    let notice_height = 140
+    if(splitString(args.author, 90).length > 1){
+        notice_height += 5
+    }
+
+    doc.setDrawColor(0)
+    doc.setFillColor(0, 46, 76)
+    doc.rect(0, 0, a4[0], banner_height, "F")
+
+    doc.addImage(args.unipi, "PNG", 15, 5, 30, 30)
+    doc.addImage(args.mlpi, "PNG", 130, 3, 30, 35)
+    doc.addImage(args.dii, "PNG", 220, 13, 60, 13)
+
+    doc.setFontSize(25);
+    doc.text(title, 150, 70, {align: "center"})
+    
+    doc.setFontSize(18);
+    doc.text(author, 150, author_height, {align: "center"})
+
+    doc.setFontSize(30);
+    doc.text(notice, 150, notice_height, {align: "center"})
+
+
+    doc.setFontSize(20);
+    doc.text(link, 150, 180, {align: "center"})
+
+    return doc
+}
+
+module.exports = {
+    buildCitationPDF: buildCitationPDF,
+    buildPreprintPDF: buildPreprintPDF
+}
